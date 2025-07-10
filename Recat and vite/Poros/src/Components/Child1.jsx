21e6b5395317116c36a2cfc24a2a946adb525c78
yashrajyadav2002 <yyash9631@gmail.com>
2025-07-10 13:27:45 +0530

    propsicons

--- Recat and vite/Poros/src/Components/Child1.jsx
+++ Recat and vite/Poros/src/Components/Child1.jsx
@@ -1,7 +1,9 @@
+import { FaBitcoin, FaChess } from "react-icons/fa";
 const Child1 = (props)=>{
     return(
         <>
-        <h1>Welcome</h1>
+        <h1>Welcome</h1><FaBitcoin color="gold" fontSize={150} />
+        <FaChess color="blue"  fontSize={100} />
         <h2>{props.name}</h2>
         <h2>{props.age}</h2>
         <h3>{props.city}</h3>
