3e7fb239047925fb90d03b37e436ea81dc975691
yashrajyadav2002 <yyash9631@gmail.com>
2025-08-08 14:18:28 +0530

    ne

--- paragram/src/App.jsx
+++ paragram/src/App.jsx
@@ -1,6 +1,7 @@
 import About from './Components/About'
 import Contact from './Components/Contact'
 import Home from './Components/Home'
+import Services from './Components/Services'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom'
@@ -12,11 +13,13 @@ export const App = () => {
     <Link to='/home/1/yash'>user 1</Link>
     <Link to='/about/2/vijay'>user 2</Link>
     <Link to='/contact/3/rohit'>user 3</Link>
+    <Link to='/services/4/sunman'>user 4</Link>
 
     <Routes>
       <Route path='/home/:id/:name' element={<Home/>} />
       <Route path='/about/:id/:name' element={<About/>} />
       <Route path='/contact/:id/:name' element={<Contact/>} />
+      <Route path='/services/:id/:name' element={<Services/>} />
     </Routes>
 
 
